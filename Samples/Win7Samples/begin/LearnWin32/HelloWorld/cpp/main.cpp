af4d792bbd85db424ab47848865816228c1134a0
Chad Bentz <1760475+felickz@users.noreply.github.com>
2025-05-22 10:35:27 -0400

    Simulate buffer overflow with wWinMain

--- Samples/Win7Samples/begin/LearnWin32/HelloWorld/cpp/main.cpp
+++ Samples/Win7Samples/begin/LearnWin32/HelloWorld/cpp/main.cpp
@@ -3,39 +3,49 @@
 #endif 
 
 #include <windows.h>
+#include <iostream>
+#include <cstring>
+
+// Vulnerable buffer overflow function for demonstration/testing only!
+void vulnerableFunction(const wchar_t* input) {
+    wchar_t buffer[8];
+    // Unsafe: No bounds checking!
+    wcscpy(buffer, input);
+    std::wcout << L"Buffer: " << buffer << std::endl;
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
+    // Simulate buffer overflow if command-line input is provided
+    if (pCmdLine && *pCmdLine) {
+        std::wcout << L"Input received: " << pCmdLine << std::endl;
+        vulnerableFunction(pCmdLine);
+    } else {
+        std::wcout << L"No input provided. Usage: HelloWorld.exe <string>" << std::endl;
+    }
 
-    // Register the window class.
+    // Register the window class
     const wchar_t CLASS_NAME[]  = L"Sample Window Class";
-    
     WNDCLASS wc = { };
-
     wc.lpfnWndProc   = WindowProc;
     wc.hInstance     = hInstance;
     wc.lpszClassName = CLASS_NAME;
-
     RegisterClass(&wc);
 
-    // Create the window.
-
+    // Create the window
     HWND hwnd = CreateWindowEx(
         0,                              // Optional window styles.
         CLASS_NAME,                     // Window class
         L"Learn to Program Windows",    // Window text
         WS_OVERLAPPEDWINDOW,            // Window style
-
-        // Size and position
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
         NULL,       // Parent window    
         NULL,       // Menu
         hInstance,  // Instance handle
         NULL        // Additional application data
-        );
+    );
 
     if (hwnd == NULL)
     {
@@ -67,15 +77,10 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
-
-            // All painting occurs here, between BeginPaint and EndPaint.
             FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
             EndPaint(hwnd, &ps);
         }
         return 0;
     }
-
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
-
-
